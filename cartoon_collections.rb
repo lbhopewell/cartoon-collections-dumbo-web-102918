03d177b5e9b583322aa28f7b2f2c6873886c1713
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 19:37:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   # Your code here
-  planeteer_calls.map! {names
+  planeteer_calls.map! {puts names.capitalize +"!"}
 end
 
 def long_planeteer_calls# code an argument here
