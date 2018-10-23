87613bb43dddff9718375b948ad72e078c31dae8
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 20:32:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ end
 def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.find {|cheese| cheese == cheese_types}
+  snacks.find {|cheese| cheese == "cheddar"|| cheese == "gouda" || cheese == "camembert"}
 end
